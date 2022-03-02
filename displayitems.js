1a1d89024abc060b1f4ab96fab000e570132aa84
Nicholas Farkas <nicholaswfarkas@gmail.com>
2022-03-01 23:18:23 -0500

    Initial Commit

---
+++ displayitems.js
@@ -0,0 +1,175 @@
+let products = [
+    {
+        ID: "1",
+        productName: "Intimate Photo(s)", 
+        productDescription: "This is just a taste of what it would be like to have me all to yourself. I am willing to bundle across my growing collection. Just let me know what you're looking for and it will be my pleasure to please you. Whether you're wanting my fat ass, big tits, cute little feet, or my wet pussy, we can satisfy each other. If you are craving more check out my OnlyFans linked below. With a membership you will have forever access to all of me and my photos, videos, and exclusive deals on all these products.",
+        productPrice: "5",
+        status: "active"
+    },
+    {
+        ID: "2",
+        productName: "Unreleased Exclusive Photo(s)", 
+        productDescription: "In my growing collections, I don't just have photos that don't make it onto OnlyFans. I have photos too kinky or risky for my average viewers. If you're not average and are looking for something hot and experiemental, check out these unrealeased collections. Of course my premium OnlyFans subscibers will always get these for free.",
+        productPrice: "10",
+        status: "active"
+    },
+    {
+        ID: "3",
+        productName: "Solo Porno", 
+        productDescription: "It can just be me and you daddy. Watch me masturbate in one of my many collections of released, unreleased, and private solo fucking sessions. Tell me exactly what you want and I promise I will give you exactly what you want. These videos will give you everything from my pussy and ass to my toes. Of course if you subscribed to my only fans linked below you could get videos multiple times a month for no extra.",
+        productPrice: "10",
+        status: "active"
+    },
+    {
+        ID: "4",
+        productName: "Private Sexting", 
+        productDescription: "I hope you understand that I have to seperate the boys that are wasting my time from the men who want to be my daddy. We can always chat and flirt but here we can go farther into our deepest fantasies together in real time. Who knows, if you get me off real good you might get yourself some free exclusive photos/videos or a free sub to my OnlyFans. But first things first, let me make you cum.",
+        productPrice: "10",
+        status: "active"
+    },
+    {
+        ID: "5",
+        productName: "Personalized Photo(s)", 
+        productDescription: "Here I can fulfill your deepest desires. You're in control, you call the shots, set the scenes, choose the angles, and I wear anything you want to see me in. If I don't have exactly what you want. Buy me any outfit and you will get discounts and more content. I'll always remember your generousity. Here you can degrade me, I degrade you, get content no one else will see for the price of lunch.",
+        productPrice: "10+",
+        status: "active"
+    },
+    {
+        ID: "6",
+        productName: "Polariod Collection", 
+        productDescription: "Are you one of my daddies that subscribe to my OnlyFans and has a favorite collection? Or a fan who just wants something tangible? You can get a whole collection of polariods shipped straight to you. You of course get the entire collection of your choosing including unreleased photos. I always use very discrete mailing techniques, personalize the package and mail everything personally myself. ;) I guess you'll just have to buy a collection to find out.",
+        productPrice: "10 + shipping",
+        status: "active"
+    },
+    {
+        ID: "7",
+        productName: "My Personally Worn Socks", 
+        productDescription: "Do you love my sweet little toes? Do you wonder how they smell or taste? Well here's your chance to get them shipped to your door daddy. I will send you one pair of socks that I have worn in my daily life. I even have some pairs I have worn for years but first come first serve ;). You can pick out the color from my drawer and I'll send you a little suprise that you'll have to order to find out. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "15 + shipping",
+        status: "active"
+    },
+    {
+        ID: "8",
+        productName: "Blowjob/Cumshot Porno", 
+        productDescription: "Have you ever wondered what it's like to get head from me? With you setting the scene, telling me how to dress, and choosing whether you want me to suck on one of my many dildos or whether I should use one of my gimps and suck on their dick for you. Either way it will be cum explosions for everyone involved especially you daddy.",
+        productPrice: "20",
+        status: "active"
+    },
+    {
+        ID: "9",
+        productName: "I Watch your Porn", 
+        productDescription: "Send me a video of your personal porno with a partner or you getting off solo. I will record my raw reaction to you. If your cock is big enough or even pathetic enough I may find myself masturbating on camera to you. The video will be sent to you and then deleted. This content is for your eyes only daddy.",
+        productPrice: "20",
+        status: "active"
+    },
+    {
+        ID: "10",
+        productName: "My Personally Worn Panties", 
+        productDescription: "Do you love my sweet pussy? Have you ever fantasized about how it smells or tastes? Well here's your chance to get it shipped to your door daddy. I will send you one pair of my panties that I have worn for at least 6 months. I even have a few that I have worn for years but first come first serve ;) You can pick out the color and style from my drawer and I'll send you a little bonus that you'll have to order to find out. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "20 + shipping",
+        status: "active"
+    },
+    {
+        ID: "11",
+        productName: "My Personally Worn Mask", 
+        productDescription: "Are you as in love with my masks as I am? Here is your chance to buy one of the masks seen in many of my instagram teases and OnlyFans photos and videos. The masks are gaurenteed to be worn by me for multiple videos, photoshoots, and personal pleasures. This is your chance to get a part of me shipped right to your door daddy. I will send you one mask that you can pick out from my collection of used masks. Of course first come first serve. I'll also send you a little bonus that you'll have to order to find out. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "30 + shipping",
+        status: "active"
+    },
+    {
+        ID: "12",
+        productName: "Personalized Film", 
+        productDescription: "Here I record a full porno where you call the shots. It can be across multiple genres of porn and with a partner or solo. A few popular examples would be me humiliating my gimp in all the ways you want, my partner humiliating me in all the ways you want, a customized jerk off instructions, me solo in a mirror doing everything I'm told. If you like what you see and would like to share, I'll post it to my OnlyFans and social media with a shoutout to you along with a discount on the price. If not I will delete the video right after I send it to you. This can be for your eyes only daddy.",
+        productPrice: "30+",
+        status: "active"
+    },
+    {
+        ID: "13",
+        productName: "Personalized Photoshoot", 
+        productDescription: "Here I take a full day photoshoot where you call the shots. It can be any type of photos you want with me wearing anything you want. If I don't have exactly what you're looking for, buy it and get photos deeply discounted and other perks. I will always remember your generousity and pay it forward. If you like what you see and would like to share, I'll post it to my OnlyFans and social media with a shoutout to you along with a dicount on the price. If not I will delete the photos right after I send it to you. This can be for your eyes only daddy.",
+        productPrice: "30+",
+        status: "active"
+    },
+    {
+        ID: "14",
+        productName: "Personalized Polariod Collection", 
+        productDescription: "Here I take a full day polariod photoshoot where you call the shots. It can be any type of photos you want with me wearing anything you want. If I don't have exactly what you're looking for, buy it and get photos deeply discounted and other perks. I will always remember your generousity and pay it forward. If you like what you see and would like to share, I'll post it to my OnlyFans and social media with a shoutout to you along with a dicount on the price. If not I will delete the photos right after I send it to you. This can be for your eyes only daddy. When mailing you your polariods, I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "30+ + shipping",
+        status: "active"
+    },
+    ,
+    {
+        ID: "15",
+        productName: "My Personally Worn Lingerie", 
+        productDescription: "Ever want a little piece of me with you at all times or a sexy outfit your partner could wear? I have a whole closet of lingerie waiting for you. I want you to think of me every time you see them and feel them every time you miss me but of course you can always send me a DM ;) You get to pick through my sizes, colors, and styles. All my cum, blood, and sweat on each item come free of charge. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "30+ + shipping",
+        status: "active"
+    },
+    {
+        ID: "16",
+        productName: "Personalized Temporary Tatoo on Me", 
+        productDescription: 'Ever think I would look better with your name or the word "slut" tattooed on my ass? I will buy very high quality temporary tattoos with whatever you want it to say and apply them to anywhere you want on my body. You can say anything, shame me, make it cute, or just your name and I will wear it for weeks (or as long as it lasts. You will be able to see it in photoshoots, videos, and even my own family will see the kinky things put on me. Of course I will send you your own exclusive photos of it on me for free. I am nervous what you do to me daddy but be creative ;)',
+        productPrice: "35",
+        status: "active"
+    },
+    {
+        ID: "17",
+        productName: "My Personally Worn Gear", 
+        productDescription: "In the market for some high quality bondage gear? How about some that me and my gimps have personally been restrained in? You'll recieve wrist and arm restraints along with a collar and ball gag. Also included is another bdsm torture device as free gift used on and or used by me. If that's not enough I will send you a exclusive photos of me wearing the gear you are recieving. All my cum, blood, and sweat on the gear come free of charge. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "50 + shipping",
+        status: "active"
+    },
+    {
+        ID: "18",
+        productName: "My Personally Used Dildo", 
+        productDescription: "Every so often I like to share a part of myself with you. These dildos have been in and out of me thousands of times and it hurts me to find them a new home because I love them so much. You will be able to pick from my selection of dildos big and small, newer and years old. Fuck yourself or your partner with a dildo that has been inside me with my pussy juice and cum all over it. Of course I will have to have one last fucking with it to say goodbye so I'll record it and send it to you. Please take care of them. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "50 + shipping",
+        status: "active"
+    },
+    {
+        ID: "19",
+        productName: "Clone a Cock", 
+        productDescription: "This is my personal favorite of what I can offer to you. I will send you a Clone a Cock free of charge. You will clone your meat and send it back to me. I will take your cock and do whatever you ask with it. Photos and Videos of all styles of your choosing. I will suck it, fuck it, or torture my gimps with it. If you would like daddy I can post the content on my OnlyFans so everyone can see me getting fucked by your cock. At the end of it all your cock will forever be in my collection and if you have a nice cock you'll see it in photos, videos, and all types of content. I can't wait for your dick to be inside me. Order now and fuck me. I always use very discrete mailing techniques, personalize the package and mail everything personally myself.",
+        productPrice: "100 + shipping",
+        status: "active"
+    },
+    {
+        ID: "20",
+        productName: "Be my Live Dungeon Master", 
+        productDescription: "Here you can show me how it is done. You will be my dungeon master for a session of BDSM play. One of my partners will be taking orders from you on a live video call. They will do whatever you want to me. Spank me, restrain me, pull my hair, eat my pussy. Show me mercy or show me pain you are in control at all times. We will have a wide range of torture tools and sex toys for you to use on me during your session. Please don't be too hard on me daddy I'll be a good slave to you I promise. Of course there are a couple guidelines that will be followed but other than that everything is on the table. I am here to serve you.",
+        productPrice: "100",
+        status: "inactive"
+    },
+    {
+        ID: "21",
+        productName: "Be my Live Dungeon Slave", 
+        productDescription: "Here I will show you how pathetic you really are. I will be your dungeon master but you will be able to set the scene however you'd like. We can either work together and punish/dominate my gimp. I will do whatever you want to them or I can take control while my gimp holds a pov camera in his mouth to livestream it to you. We will have a wide range of torture tools and sex toys for me to use during your session. I need my gimps unfortunately so I have to set a couple guidelines but everything else is on the table.",
+        productPrice: "100",
+        status: "inactive"
+    }
+];
+
+$(document).ready(function() {
+
+    products.forEach(function(product){
+        stock = "";
+        stockMessage = "";
+        if(product.status == "inactive")
+        {
+            stock = " - Out Of Stock"
+            stockMessage = "<br><br>&#128561 I'm not ready for this yet. I promise it will be back very soon as I prepare myself for you &#129316<br>I'd love to chat about it &#128524"
+        }
+        $(`
+        <div class="tippedProduct">
+            <span class="productTitle product${product.ID}">&#10083; ${product.productName}${stock}</span>
+            <div class="productInfo${product.ID} hide">
+                <p class="productDescription">${product.productDescription}${stockMessage}</p><br>
+                <span class="productPrice">${product.productPrice} USD</span>
+            </div>
+        </div><br>`).appendTo('#tippedProducts');
+
+        $(".product"+product.ID).click(function(){
+            $(".productInfo"+product.ID).toggle("slow");
+        });
+    });
+});
